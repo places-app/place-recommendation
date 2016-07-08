4b8fe3f492f69bef31938120120e3e9770561780
Andrew Phavichitr <andrew.phavichitr@gmail.com>
2016-07-07 20:51:33 -0700

    Add new api to get place id and update controller to update place id

--- controllers/PlaceDetailsController.js
+++ controllers/PlaceDetailsController.js
@@ -4,6 +4,69 @@ const api = require('../utils/api');
 const client = require('../db/redis');
 let query;
 
+const insertPlaceTypes = (placeId, typeId, typeCallback) => {
+  // insert type into placeTypes table
+  query = `INSERT INTO "placeTypes" ("placeId", "typeId", "createdAt", "updatedAt") SELECT ${placeId}, ${typeId}, current_timestamp, current_timestamp WHERE NOT EXISTS (SELECT * FROM "placeTypes" WHERE "placeId" = ${placeId} AND "typeId" = ${typeId})`;
+  Sequelize.query(query)
+  .then(() => {
+    // end of one inner async iteration
+    typeCallback();
+  })
+  .catch(err => {
+    console.log('Error inserting place type: ', err);
+    // end of one inner async iteration
+    typeCallback();
+  });
+};
+
+// api call to google places api with google place id
+const getPlaceAPIDetails = (placeId, gPlaceId, placeCallback) => {
+  api.getPlaceDetails(gPlaceId)
+  .then(res => {
+    const types = res.data.result.types;
+    // iterate through each type
+    async.eachSeries(types, (type, typeCallback) => {
+      // check if type exists
+      query = `SELECT id FROM types WHERE name = '${type}'`;
+      // query for all types where type matches
+      Sequelize.query(query)
+      .spread(typeResults => {
+        let typeId;
+        // if type doesn't exist, then insert into types table
+        if (typeResults.length === 0) {
+          query = `INSERT INTO types (name, "createdAt", "updatedAt") VALUES ('${type}', current_timestamp, current_timestamp) RETURNING id`;
+          Sequelize.query(query)
+          .then(typeInserted => {
+            typeId = typeInserted[0].id;
+            insertPlaceTypes(placeId, typeId, typeCallback);
+          })
+          .catch(err => {
+            console.log('Error inserting type: ', err);
+          });
+        // if type exists, then insert into placeTypes table
+        } else {
+          typeId = typeResults[0].id;
+          insertPlaceTypes(placeId, typeId, typeCallback);
+        }
+      })
+      .catch(err => {
+        console.log('Error querying type: ', err);
+      });
+    }, err => {
+      if (err) {
+        console.log('Error: ', err);
+      } else {
+        console.log('Done inserting types and place types.');
+        // end of one outer async iteration
+        placeCallback();
+      }
+    });
+  })
+  .catch(err => {
+    console.log('Error fetching place details: ', err);
+  });
+};
+
 module.exports = {
   getPlaceDetails: (prevPlaceId) => {
     query = `SELECT * FROM places WHERE id >= ${prevPlaceId}`;
@@ -15,88 +78,35 @@ module.exports = {
       async.eachSeries(places, (place, placeCallback) => {
         const placeId = place.id;
         let gPlaceId = place.gPlaceId;
+        const name = encodeURIComponent(place.name);
         const lat = place.lat;
         const lng = place.lng;
         // set redis placeId to placeId from psql
         rPlaceId = placeId;
-        console.log('gPlaceId', gPlaceId);
-        if (!gPlaceId) {
-          // async
-          api.getPlaceId(lat, lng)
+        if (gPlaceId === null) {
+          api.getPlaceId(name, lat, lng)
           .then(res => {
-            console.log('new gPlaceId', res.data.results[0].place_id);
-            gPlaceId = res.data.results[0].place_id;
-            query = `UPDATE places SET gPlaceId = ${gPlaceId} WHERE id = ${placeId}`;
-          })
-          .catch(err => {
-            console.log('Error updating places: ', err);
-          });
-        }
-        // api call to google places api with google place id
-        console.log('end get g place id');
-        api.getPlaceDetails(gPlaceId)
-        .then(res => {
-          const types = res.data.result.types;
-          // iterate through each type
-          async.eachSeries(types, (type, typeCallback) => {
-            // check if type exists
-            query = `SELECT id FROM types WHERE name = '${type}'`;
-            // query for all types where type matches
-            Sequelize.query(query)
-            .spread(typeResults => {
-              let typeId;
-              // if type doesn't exist, then insert into types table
-              if (typeResults.length === 0) {
-                query = `INSERT INTO types (name, "createdAt", "updatedAt") VALUES ('${type}', current_timestamp, current_timestamp) RETURNING id`;
-                Sequelize.query(query)
-                .then(typeInserted => {
-                  typeId = typeInserted[0].id;
-                  // insert type into placeTypes table
-                  query = `INSERT INTO "placeTypes" ("placeId", "typeId", "createdAt", "updatedAt") SELECT ${placeId}, ${typeId}, current_timestamp, current_timestamp WHERE NOT EXISTS (SELECT * FROM "placeTypes" WHERE "placeId" = ${placeId} AND "typeId" = ${typeId})`;
-                  Sequelize.query(query)
-                  .then(() => {
-                    typeCallback();
-                  })
-                  .catch(err => {
-                    console.log('Error inserting place type: ', err);
-                    typeCallback();
-                  });
-                })
-                .catch(err => {
-                  console.log('Error inserting type: ', err);
-                });
-              // if type exists, then insert into placeTypes table
-              } else {
-                typeId = typeResults[0].id;
-                query = `INSERT INTO "placeTypes" ("placeId", "typeId", "createdAt", "updatedAt") SELECT ${placeId}, ${typeId}, current_timestamp, current_timestamp WHERE NOT EXISTS (SELECT * FROM "placeTypes" WHERE "placeId" = ${placeId} AND "typeId" = ${typeId})`;
-                Sequelize.query(query)
-                .then(() => {
-                  // end of one inner async iteration
-                  typeCallback();
-                })
-                .catch(err => {
-                  console.log('Error inserting place type: ', err);
-                  // end of one inner async iteration
-                  typeCallback();
-                });
-              }
-            })
-            .catch(err => {
-              console.log('Error querying type: ', err);
-            });
-          }, err => {
-            if (err) {
-              console.log('Error: ', err);
+            if (res.data.predictions.length > 0) {
+              gPlaceId = res.data.predictions[0].place_id;
+              query = `UPDATE places SET "gPlaceId" = '${gPlaceId}' WHERE id = ${placeId}`;
+              Sequelize.query(query)
+              .then(() => {
+                getPlaceAPIDetails(placeId, gPlaceId, placeCallback);
+              })
+              .catch(err => {
+                console.log('Error updating place: ', err);
+              });
             } else {
-              console.log('Done inserting types and place types.');
               // end of one outer async iteration
               placeCallback();
             }
+          })
+          .catch(err => {
+            console.log('Error fetching place id: ', err);
           });
-        })
-        .catch(err => {
-          console.log('Error fetching place details: ', err);
-        });
+        } else {
+          getPlaceAPIDetails(placeId, gPlaceId, placeCallback);
+        }
       }, err => {
         if (err) {
           console.log('Error: ', err);
---
+++ tests/places-api-test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const expect = chai.expect;
+chai.should();
+let request = require('supertest');
+
+//request = request(appUrl);
+
+describe('Get Google Place ID from Geocode API', () => {
+  it('should return Google Place ID', () => {
+
+
+    // expect().to.equal();
+  });
+});
